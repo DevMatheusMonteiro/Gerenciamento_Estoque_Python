b9b8b2b220c2a51309b6170acfc67416ea5eb0d6
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-12-03 16:45:37 -0300

    adicinando as funções filtrarPorLimiteDeQuantidade() e atualizarPreco()

--- repository.py
+++ repository.py
@@ -9,12 +9,16 @@ def listar():
 def buscarPorCodigo(codigo):
     return next((produto for produto in estoque_lista if produto["codigo"] == codigo))
 def buscarPorDescricao(descricao):
-    return filter(lambda produto:produto["descricao"].find(descricao)!=-1,estoque_lista)
+    return list(filter(lambda produto:produto["descricao"].find(descricao)!=-1,estoque_lista))
 def ordenarPorQuantidade(produtos, decrescente=False):
     return sorted(produtos,key=lambda produto:produto["quantidade"],reverse=decrescente)
+def filtrarPorLimiteDeQuantidade(quantidade=100):
+    return list(filter(lambda produto:produto["quantidade"])<=quantidade)
 def remover(produto):
     estoque_lista.remove(produto)
 def aumentarQuantidade(produto):
     produto["quantidade"]+=1
 def diminuirQuantidade(produto):
     produto["quantidade"]-=1
+def atualizarPreco(produto, preco):
+    produto["precoVenda"] = preco
