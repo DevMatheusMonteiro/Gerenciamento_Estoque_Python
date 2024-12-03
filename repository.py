34c9d61d4497ce80a749b72ffdc174022b21799d
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-12-03 12:19:46 -0300

    criação do crud no repository

--- repository.py
+++ repository.py
@@ -1,5 +1,20 @@
 """ 
 # Camada para intermediária entre a aplicação e a fonte de dados
 """
-from estoque import estoque_inicial
-# def findAll():
+from estoque import estoque_lista
+def criar(produto):
+    estoque_lista.append(produto)
+def listar():
+    return estoque_lista
+def buscarPorCodigo(codigo):
+    return next((produto for produto in estoque_lista if produto["codigo"] == codigo))
+def buscarPorDescricao(descricao):
+    return filter(lambda produto:produto["descricao"].find(descricao)!=-1,estoque_lista)
+def ordenarPorQuantidade(produtos, decrescente=False):
+    return sorted(produtos,key=lambda produto:produto["quantidade"],reverse=decrescente)
+def remover(produto):
+    estoque_lista.remove(produto)
+def aumentarQuantidade(produto):
+    produto["quantidade"]+=1
+def diminuirQuantidade(produto):
+    produto["quantidade"]-=1
