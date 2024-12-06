f94c33fc71e46eb73b487567cc0cc3265db106ed
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-12-05 21:07:22 -0300

    adicionando classe produto

---
+++ model/produto.py
@@ -0,0 +1,9 @@
+class Produto:
+    def __init__(self,descricao: str,codigo: int,quantidade: int,custoItem: float,precoVenda: float):
+        self.descricao = descricao
+        self.codigo = codigo
+        self.quantidade = quantidade
+        self.custoItem = custoItem
+        self.precoVenda = precoVenda
+    def __str__(self):
+        return f"Código: {self.codigo}\nDescrição: {self.descricao}\nQuantidade: {self.quantidade}\nCusto do Item: {self.custoItem}\nPreço de Venda: {self.precoVenda}\nValor Total: {self.precoVenda * self.quantidade}"
