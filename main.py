659f539fc2c3290e6551923695f56c002da23fbd
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-12-07 12:28:06 -0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from repositories.produtoRepository import ProdutoRepository
 from utils.produtoUtil import ProdutoUtil
-from services.produtoService import ProdutoService
-# print(ProdutoUtil.criar())
-produtoService = ProdutoService()
-produtoService.buscarPorDescricao("Notebook")
-print(produtoService.totalDeLucroPresumidoEmEstoque())
+ProdutoUtil.menu()
